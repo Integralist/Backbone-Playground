0c62111e6a7fcc56083470b7b83a400e9621d82a
Mark McDonnell <mark.mcdx@gmail.com>
2012-08-05 15:56:50 +0100

    Fix some Model access issue

--- Assets/Includes/Contacts.php
+++ Assets/Includes/Contacts.php
@@ -1,24 +1,27 @@
 <?php
-	$json = array(
-		array(
-			"name" => "Joe Bloggs",
-			"age" => 12, 
-			"address" => "9 Cables Street, London",
-			"role" => "Manager"
-		),
-		array(
-			"name" => "Dan Smith",
-			"age" => 23, 
-			"address" => "Bambridge Road, Essex",
-			"role" => "Developer"
-		),
-		array(
-			"name" => "Bradley Few",
-			"age" => 22, 
-			"address" => "Meeson Mead, Leads",
-			"role" => "Developer"
-		)
-	);
-	
-	echo json_encode($json);
+    $json = array(
+        array(
+            "id" => 00
+            "name" => "Joe Bloggs",
+            "age" => 12, 
+            "address" => "9 Cables Street, London",
+            "role" => "Manager"
+        ),
+        array(
+            "id" => 01
+            "name" => "Dan Smith",
+            "age" => 23, 
+            "address" => "Bambridge Road, Essex",
+            "role" => "Developer"
+        ),
+        array(
+            "id" => 02
+            "name" => "Bradley Few",
+            "age" => 22, 
+            "address" => "Meeson Mead, Leads",
+            "role" => "Developer"
+        )
+    );
+    
+    echo json_encode($json);
 ?>
